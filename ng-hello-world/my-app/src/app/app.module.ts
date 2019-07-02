9f69bd21538941c8e833a1041eb4be083ddaedb6
kavi <fivekavi@gmail.com>
2019-07-02 16:03:04 +0530

    changed

---
+++ ng-hello-world/my-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HelloWorldNewComponent } from './hello-world-new/hello-world-new.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserItemComponent,
+    UserListComponent,
+    HelloWorldNewComponent,
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
